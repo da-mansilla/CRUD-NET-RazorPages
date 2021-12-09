72c3a7e0cd85cd0d62622b61b37c9eee0d6b8a10
Agustin Mansilla <mansillaagustin1019@gmail.com>
2021-12-09 16:03:45 -0300

    addNotificationsAndAlert

--- BookListRazor/Startup.cs
+++ BookListRazor/Startup.cs
@@ -26,6 +26,7 @@ namespace BookListRazor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllersWithViews();
             services.AddRazorPages().AddRazorRuntimeCompilation();
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +52,7 @@ namespace BookListRazor
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
         }
